22958f9b70ff836cb7c68ef56345767d881dea38
Jamie Redmond <jamie.redmond@speech-graphics.com>
2026-03-03 14:18:35 +0100

    Add build.bat for Windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+SET UE_DIR=C:\Program Files\Epic Games\UE_5.7\Engine\
+
+CALL "%UE_DIR%Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%~dp0FaceFX.uplugin" -Package="%~dp0Packages\Windows\FaceFX" -Installed -NoCompile -TargetPlatforms=Win64
